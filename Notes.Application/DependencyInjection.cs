1797869c0a5f31bc5adb6a5cba6562e3bc7f9487
SoloVova <vsolo0@gmail.com>
2021-12-14 13:20:54 +0200

    0.09 Validation

--- Notes.Application/DependencyInjection.cs
+++ Notes.Application/DependencyInjection.cs
@@ -1,11 +1,15 @@
 ﻿using System.Reflection;
+using FluentValidation;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using Notes.Application.Behaviors;
 
 namespace Notes.Application{
     public static class DependencyInjection{
         public static IServiceCollection AddApplication(this IServiceCollection services){
             services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssemblies(new[]{Assembly.GetExecutingAssembly(),});
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
             return services;
         }
     }
