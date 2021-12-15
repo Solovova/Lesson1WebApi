458905f8a0214ed1ca052c18bf72ba3a88e005ee
SoloVova <vsolo0@gmail.com>
2021-12-15 14:16:32 +0200

    0.12 Tests

---
+++ Notes.Tests/Notes/Queries/GetNoteListQueryHandlerTests.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using System.Threading;
+using System.Threading.Tasks;
+using Notes.Application.Notes.Queries.GetNoteList;
+using Notes.Persistence;
+using Notes.Tests.Common;
+using Shouldly;
+using Xunit;
+
+namespace Notes.Tests.Notes.Queries
+{
+    [Collection("QueryCollection")]
+    public class GetNoteListQueryHandlerTests
+    {
+        private readonly NotesDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetNoteListQueryHandlerTests(QueryTestFixture fixture)
+        {
+            _context = fixture.Context;
+            _mapper = fixture.Mapper;
+        }
+
+        [Fact]
+        public async Task GetNoteListQueryHandler_Success()
+        {
+            // Arrange
+            var handler = new GetNoteListQueryHandler(_context, _mapper);
+
+            // Act
+            var result = await handler.Handle(
+                new GetNoteListQuery
+                {
+                     UserID = NotesContextFactory.UserBId
+                },
+                CancellationToken.None);
+
+            // Assert
+            result.ShouldBeOfType<NoteListVm>();
+            result.Notes.Count.ShouldBe(2);
+        }
+    }
+}
